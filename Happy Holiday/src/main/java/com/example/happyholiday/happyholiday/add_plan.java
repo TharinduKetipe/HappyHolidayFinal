fce72f10836860ef9639ee93b6dff23ff4b06734
tharinduketipe <katramesh@gmail.com>
2015-04-28 14:05:15 +0530

    add plan changed

--- Happy Holiday/src/main/java/com/example/happyholiday/happyholiday/add_plan.java
+++ Happy Holiday/src/main/java/com/example/happyholiday/happyholiday/add_plan.java
@@ -2,6 +2,10 @@ package com.example.happyholiday.happyholiday;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 /**
  * Created by tharindu on 4/23/15.
@@ -10,5 +14,15 @@ public class add_plan extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        EditText pln_name_txt = (EditText)findViewById(R.id.ap_tf_pln_name);
+        DatePicker pln_date = (DatePicker) findViewById(R.id.ap_pln_date);
+        TimePicker pln_time = (TimePicker) findViewById(R.id.ap_pln_time);
+        EditText pln_desc = (EditText) findViewById(R.id.ap_ti_desc);
+        Button pln_bt_home = (Button) findViewById(R.id.ap_bt_home);
+        Button pln_bt_add = (Button) findViewById(R.id.ap_bt_add);
+        Button pln_bt_cancel = (Button) findViewById(R.id.ap_bt_cancel);
+
+
     }
 }
